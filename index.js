a7e9d0169cf20e2c02e0ff4f3defeb16655e8335
Andrey <soul.evans.15@gmail.com>
2019-01-30 19:36:08 +0300

    replaced jQuery with pure js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-    console.log(req);
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+io.on('connection', function(socket) {
+    socket.on('message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('message', msg);
     });
 });
 
