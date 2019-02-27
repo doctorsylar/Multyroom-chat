1ca0251a58a8c364cc95fb8f0f44d9973290d0e0
Andrey <soul.evans.15@gmail.com>
2019-02-27 03:28:32 +0300

    chat overflow

--- server.js
+++ server.js
@@ -67,6 +67,7 @@ io.on('connection', function(socket) {
 http.listen(port, function(){
     console.log('listening on port: ' + port);
 });
+// creating socket objects for rooms which exists at app's start
 for (let room of roomsList) {
     roomObjectsList[room] = createCustomSocket('/' + room)
 }
